22ccad265ee05c8536bb7be8cad119076665b28f
patillacode <patillacode@gmail.com>
2016-05-28 14:28:01 +0100

    Added stup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+
+from setuptools import setup, find_packages
+
+setup(
+    name='tilt-o-meter',
+    version='0.1.0',
+    description='',
+    license='GPLv2',
+    packages=find_packages(),
+    author='patillacode',
+    author_email='patillacode@gmail.com',
+    url='https://github.com/patillacode/tilt-o-meter',
+    install_requires=['bumpversion>=0.5.3',
+                      'Flask>=0.10.1',
+                      'gitflow>=0.5.1',
+                      'riotwatcher>=1.3.2'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: GNU General Public License v2',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7']
+)
