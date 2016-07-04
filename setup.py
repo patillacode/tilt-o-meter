45cb882cf3351379d93a6a4c70f97545a231a837
Gonzalo Fernandez <gonzalo.fernandez@imagination.com>
2016-07-04 13:20:32 +0100

    Bump version: 0.1.1 → 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tilt-o-meter',
-    version='0.1.1',
+    version='0.1.2',
     description='',
     license='GPLv2',
     packages=find_packages(),
