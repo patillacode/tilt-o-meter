780931226562c8f294a237128e6c668f1e1096a2
Gonzalo Fernandez <gonzalo.fernandez@imagination.com>
2016-07-28 15:58:23 +0100

    Fixed algorithm.

--- tiltometer/tiltometer.py
+++ tiltometer/tiltometer.py
@@ -43,7 +43,7 @@ def get_stats(games, champions_dict):
                                                 champions_dict)
         champion_key = get_champion_key_by_id(game['championId'],
                                               champions_dict)
-        champion_img = "{0}{1}.png".format(config.CHAMPION_ICON_URL,
+        champion_img = '{0}{1}.png'.format(config.CHAMPION_ICON_URL,
                                            champion_key)
         position = game['stats'].get('playerPosition', 0)
         pentakill = True if game['stats'].get('largestMultiKill', 0) == 5 \
@@ -107,10 +107,10 @@ def get_tilt_level(games):
 
     for game in games:
         # When practising vs BOTS or custom games you never get tilted,
-        # so they don't count. We even take 5 points off the tilt level,
+        # so they don't count. We even take 3 points off the tilt level,
         # since they are just for fun
         if ('BOT' or 'NONE') in game['subType']:
-            tilt_points -= 5
+            tilt_points -= 3
             continue
 
         this_game_tilt_points = 0
@@ -118,9 +118,9 @@ def get_tilt_level(games):
         if not game['stats']['win']:
             # 0.5 tilt points for every game lost
             # bigger multiplier the more recent the game was
-            this_game_tilt_points += 0.5 * multiplier
-            tilt_points += 0.5 * multiplier
-            # add cold strak points
+            this_game_tilt_points += multiplier
+            tilt_points += multiplier
+            # add cold streak
             cold_streak += 1
 
             # if you lost a game under 25 minutes probably means you forfited
@@ -132,12 +132,19 @@ def get_tilt_level(games):
         else:
             cold_streak = 0
 
+            # if you won a game under 25 minutes probably means the
+            # opponents forfited or you crushed them
+            # multiplier for the more recent the game was
+            if game['stats']['timePlayed'] < 1500:
+                tilt_points -= multiplier
+                this_game_tilt_points -= multiplier
+
         # if you lost 2 or more games in a row you gain tilt points per game
         # bigger multiplier the more recent the cold strike was
         if cold_streak > 1:
-            tilt_points += cold_streak * (multiplier / 2)
+            tilt_points += cold_streak * (multiplier)
 
-            this_game_tilt_points += cold_streak * (multiplier / 2)
+            this_game_tilt_points += cold_streak * (multiplier)
 
         kda = get_kda(game['stats'].get('championsKilled', 0),
                       game['stats'].get('numDeaths', 0),
@@ -145,19 +152,27 @@ def get_tilt_level(games):
 
         # if your KDA is low... tilt points for you!
         if kda < 1:
-            tilt_points += multiplier
-            this_game_tilt_points += multiplier
+            # tilt_points += 1.25 * multiplier
+            tilt_points += 10
+            # this_game_tilt_points += 1.25 * multiplier
+            this_game_tilt_points += 10
         elif kda < 2:
-            tilt_points += 0.75 * multiplier
-            this_game_tilt_points += 0.75 * multiplier
+            # tilt_points += 0.75 * multiplier
+            tilt_points += 5
+            # this_game_tilt_points += 0.75 * multiplier
+            this_game_tilt_points += 5
         elif kda < 3:
-            tilt_points += 0.5 * multiplier
-            this_game_tilt_points += 0.5 * multiplier
+            # tilt_points += 0.5 * multiplier
+            tilt_points += 2
+            # this_game_tilt_points += 0.5 * multiplier
+            this_game_tilt_points += 2
         else:
             # if your kda is over 3 you did very well and you are happy
             # let's get some tilt point off of you ^^
-            tilt_points -= multiplier
-            this_game_tilt_points -= multiplier
+            # tilt_points -= multiplier
+            tilt_points -= 1
+            # this_game_tilt_points -= multiplier
+            this_game_tilt_points -= 1
 
         # When playing a normal game tilt exists but is not the same as rankeds
         # since you play just to practice, sometimes troll etc...
